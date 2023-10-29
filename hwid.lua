8e6fb3a61e40d6a4a1b2e488e6e55d289e71dedb
SpxlZ <146632787+SpxlZ@users.noreply.github.com>
2023-10-29 21:40:00 +0100

    Update hwid.lua

--- hwid.lua
+++ hwid.lua
@@ -1,4 +1,4 @@
-60380c66-e6fa-4a29-a51a-fab93e927445 --SpxlzHWID
+60380c66-e6fa-4a29-a51a-fab93e927445
 8e14a6ad-dbf6-44cc-850c-ee2974d8af78
-c9c485ca-e774-4848-82c0-247cfd8aceb4 --JaydenHWID
+c9c485ca-e774-4848-82c0-247cfd8aceb4
 
